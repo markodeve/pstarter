2b490fc281b5f92ac7d0bbc3e504038ea00ca981
leomark92 <markobogush@gmail.com>
2021-07-13 15:10:03 +0300

    miitaaDelab1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,25 @@
-let btn = document.querySelector('.dora__btn');
+// let btn = document.querySelector('.dora__btn');
 
-btn.addEventListener('click', (ev) => {
-     console.log(ev);
-});
+// btn.addEventListener('click', (ev) => {
+//      console.log(ev);
+// });
+
+// let naame = prompt("please say your name");
+
+// let user;
+// let user2;
+
+// user = naame.toLowerCase();
+
+// user2 = user[0].toUpperCase();
+
+// alert(`hello ${user2 + user.slice(1)}`);
+
+// let messInp = prompt("");
+
+// alert(messInp.trim());
+
+let score = prompt("score");
+
+console.log(score.padStart(5, 0));
+console.log(score.padEnd(5, 0));

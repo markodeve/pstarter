100ef46a756ed5a8a6b451e2d46a1b1231cd4c84
leomark92 <markobogush@gmail.com>
2021-09-29 21:29:15 +0300

    dorbtnadd1.9binapi11

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,16 @@
 const dorador = document.getElementById("count_el"),
       btnn = document.getElementById("btnn");
 
+btnn.addEventListener('click', () => {
+    dorador.innerText = 5;
+});
+
+// let unanderstandable = 5;
+// let alsodonno = 7;
+// let count = unanderstandable + alsodonno;
+// console.log(count);
+
+
 
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,16 @@ const dorador = document.getElementById("count_el"),
       btnn = document.getElementById("btnn");
 
 btnn.addEventListener('click', () => {
-    console.log('dor');
+    dorador.innerText = 5;
 });
+
+// let unanderstandable = 5;
+// let alsodonno = 7;
+// let count = unanderstandable + alsodonno;
+// console.log(count);
+
+
+
 
 
 

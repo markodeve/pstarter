d60742f6e181f626d04bb14e83582ba00ce7d8fe
leomark92 <markobogush@gmail.com>
2021-09-29 19:35:25 +0300

    dorbtnadd1.9binapi

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,37 +1,8 @@
-// let btn = document.querySelector('.dora__btn');
-
-// btn.addEventListener('click', (ev) => {
-//      console.log(ev);
-// });
-
-// let naame = prompt("please say your name");
-
-// let user;
-// let user2;
-
-// user = naame.toLowerCase();
-
-// user2 = user[0].toUpperCase();
-
-// alert(`hello ${user2 + user.slice(1)}`);
-
-// let messInp = prompt("");
-
-// alert(messInp.trim());
-
-// let score = prompt("score");
-
-// console.log(score.padStart(5, 0));
-// console.log(score.padEnd(5, 0));
-
-// let wordWB = "PC adress state word planned presence";
-
-// console.log(wordWB.charAt(0));
-
-// let arrayToV = 'bin fetch synch rerun api';
+const dorador = document.getElementById("count_el"),
+      btnn = document.getElementById("btnn");
 
 
-// let MSet = arrayToV.split(' ');
 
-// console.log(MSet);
+
+
 

5912743e5efd2602c9d10f3217ea3a760a1dc660
leomark92 <markobogush@gmail.com>
2021-10-04 16:13:08 +0300

    taktikadengi3.5

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,32 @@
 const dorador = document.getElementById("count_el"),
-      btnn = document.getElementById("btnn");
+      btnn = document.getElementById("btnn"),
+      savebtn = document.getElementById("save");
 
-// btnn.addEventListener('click', () => {
-//     dorador.innerText = 5;
-// });
-
-// let unanderstandable = 5;
-// let alsodonno = 7;
-// let count = unanderstandable + alsodonno;
-// console.log(count);
+      let totalCount = 0;
 
 
+    function incrementCount() {
+        totalCount += 1;
+}
+
+btnn.addEventListener('click', () => {
+    incrementCount();
+    dorador.innerText = totalCount;
+});
+
+function save() {
+   console.log(totalCount);
+}
+
+savebtn.addEventListener('click', () => {
+    save();
+});
 
 
+let username = "Dor";
+let message = "you have threee new dors.";
+
+console.log(username + ", " + message);
 
 
 
